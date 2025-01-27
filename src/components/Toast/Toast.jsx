60275664d5f74a8a0a20b85fc4cac08212fe7747
Seokjun Kang <kangsj0327@gmail.com>
2025-01-27 19:55:17 +0900

    feat: Toast 레이아웃, 스타일 구현

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -1,5 +1,15 @@
-function Toast() {
-  return <>toast!</>;
+import { Container, Content, Message, Completed, Button } from "./Toast.style";
+
+function Toast({ onClose }) {
+  return (
+    <Container>
+      <Content>
+        <Completed />
+        <Message>URL이 복사되었습니다.</Message>
+      </Content>
+      <Button onClick={onClose} />
+    </Container>
+  );
 }
 
 export default Toast;
--- src/components/Toast/Toast.style.js
+++ src/components/Toast/Toast.style.js
@@ -1,3 +1,36 @@
 import styled from "styled-components";
+import Completed from "../../assets/icons/completed.svg";
+import Close from "../../assets/icons/close.svg";
 
-const Container = styled.div``;
+export const Container = styled.div`
+  width: 524px;
+  height: 64px;
+  background-color: #000000cc;
+  border-radius: 8px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0 30px;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const Message = styled.div`
+  ${({ theme }) => theme.fonts["16r"]};
+  color: ${({ theme }) => theme.colors.white};
+
+  user-select: none;
+`;
+
+export const Button = styled(Close)`
+  cursor: pointer;
+`;
+
+export { Completed };

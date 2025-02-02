b6e6b8167442cb70f510ffbdf107fc2afbdf4233
heresue <sue.synpark@gmail.com>
2025-02-03 01:15:00 +0900

    Dropdown 기능 구현

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,7 +1,34 @@
-import { ErrorMessage } from "./TextField.style";
+import { useState } from "react";
+import { ErrorMessage, Options, Option } from "./TextField.style";
 
 function Dropdown() {
-  return;
+  const [error, setError] = useState(false);
+  const [value, setValue] = useState("placeholder");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    setError(e.target.value === "");
+  };
+
+  return (
+    <>
+      {/* default width: 100% */}
+      <Options
+        width="300px"
+        value={value}
+        onChange={handleChange}
+        error={error}
+      >
+        <Option value="placeholder" disabled hidden>
+          Placeholder
+        </Option>
+        <Option>1번</Option>
+        <Option>2번</Option>
+        <Option>3번</Option>
+      </Options>
+      {error && <ErrorMessage>값을 입력해 주세요.</ErrorMessage>}
+    </>
+  );
 }
 
 export default Dropdown;

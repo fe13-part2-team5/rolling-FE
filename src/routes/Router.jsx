f2de8ce98ea9ad98ae6b65befbb9d31b6bb86209
Seokjun Kang <kangsj0327@gmail.com>
2025-02-05 17:01:37 +0900

    feat: edit page 라우터 생성

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Post from "../pages/Post";
+import Edit from "../pages/Edit";
 
 function Router() {
   return (
@@ -8,6 +9,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post" element={<Post />} />
+        <Route path="/post/:id/edit" element={<Edit />} />
       </Routes>
     </BrowserRouter>
   );

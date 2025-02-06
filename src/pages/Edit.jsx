2e28bc1c9a045f8553756b1e6804c7c4c40ae23e
Seokjun Kang <kangsj0327@gmail.com>
2025-02-06 11:33:26 +0900

    feat: Edit 페이지 롤링 페이퍼 삭제 기능 구현

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -55,15 +55,28 @@ function Edit() {
   const baseURL = import.meta.env.VITE_API_URL;
   const navigate = useNavigate();
   const { id } = useParams();
-  const handleDeleteRecipient = () => {
-    axios
-      .delete(`${baseURL}/recipient/${id}`)
-      .then((response) => {
-        console.log(response);
-      })
-      .finally(() => {
-        navigate(`/list`);
-      });
+
+  const deleteRecipient = async (recipientId) => {
+    try {
+      const response = await axios.delete(
+        `${baseURL}/recipients/${recipientId}`
+      );
+      if (response.status === 200 || response.status === 204) {
+        return { success: true };
+      } else {
+        return { success: false };
+      }
+    } catch (error) {
+      console.error("롤링 페이퍼 대상 삭제 중 에러 발생:", error);
+      return { success: false };
+    }
+  };
+
+  const handleClick = async () => {
+    const response = await deleteRecipient(id);
+    if (response.success) {
+      navigate("/list");
+    }
   };
 
   return (
@@ -71,7 +84,7 @@ function Edit() {
       <Header />
       <HeaderService />
       <div>
-        <button onClick={handleDeleteRecipient}>삭제하기</button>
+        <button onClick={handleClick}>삭제하기</button>
         <CardList cards={mockData} />
       </div>
     </>

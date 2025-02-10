60ccaa4f6aa7c0f01fa39ad9c0b02db040de5182
heresue <sue.synpark@gmail.com>
2025-02-10 20:11:26 +0900

    머지 오류 수정

--- src/pages/PostCreate.jsx
+++ src/pages/PostCreate.jsx
@@ -4,7 +4,7 @@ import PrimaryButton from "../components/Buttons/PrimaryButton";
 import ToggleButton from "../components/Buttons/ToggleButton";
 import { InputField } from "../components/TextField/TextField";
 import * as P from "./PostAndMessage.style";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
@@ -15,34 +15,6 @@ function PostCreate() {
   const [selectedColor, setSelectedColor] = useState("beige");
   const [selectedImageURL, setSelectedImageURL] = useState(null);
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const preloadImages = (imageUrls) => {
-      imageUrls.forEach((url) => {
-        const img = new Image();
-        img.src = url;
-      });
-    };
-
-    const getBackgroundImage = async () => {
-      try {
-        const response = await axios.get(
-          "https://rolling-api.vercel.app/background-images/"
-        );
-        const imageUrls = response.data.imageUrls;
-        setImages(imageUrls);
-        preloadImages(imageUrls);
-
-        if (!selectedImageURL) {
-          setSelectedImageURL(imageUrls[0]);
-        }
-      } catch (error) {
-        console.error("이미지 로드 실패:", error);
-      }
-    };
-
-    getBackgroundImage();
-  }, [selectedImageURL]);
 
   const isButtonDisabled = !name.trim();
 
